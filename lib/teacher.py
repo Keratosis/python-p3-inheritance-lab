522a56bfc9cfcf897682586fc2ff20c132013b46
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-19 01:03:41 +0300

    Assignment done

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,21 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Default knowledge"]
 
-    def teach(self):
-        pass
+    def teach(self, information=None):
+        if information is None:
+            print("No information provided. Teaching cannot be performed.")
+            return None
+        self.knowledge.append(information)
+        return information
+
+
+
+
+T = Teacher("braxton","shaffie")
+T.teach()
+print(T.first_name,T.last_name)
+print(T.knowledge)
