a84d51f530acd53d2e159ab23b1d670508756743
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-23 01:08:38 +0300

    updating

--- lib/teacher.py
+++ lib/teacher.py
@@ -7,19 +7,24 @@ import random
 class Teacher(User):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
-        self.knowledge = ["Default knowledge"]
+        self.knowledge = ["str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",]
 
-    def teach(self, information=None):
-        if information is None:
+    def teach(self):
+        if self.knowledge:
+            return self.knowledge[0]
+        else:
             print("No information provided. Teaching cannot be performed.")
-            return None
-        self.knowledge.append(information)
-        return information
-
 
 
 
 T = Teacher("braxton","shaffie")
 T.teach()
 print(T.first_name,T.last_name)
-print(T.knowledge)
+print(T.knowledge[3])
